2b54d6e18a88c52a6355365f07de01e79accd309
unknown <malay.nayak@TECH-LP-145.techaspect.com>
2017-09-25 16:29:32 +0530

    Sorting and paging added

--- contact_manager/src/ContactListBuilder.php
+++ contact_manager/src/ContactListBuilder.php
@@ -6,6 +6,7 @@ use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
 use Drupal\Core\Link;
 use Drupal\Core\Url;
+use Drupal;
 
 /**
  * Defines a class to build a listing of Contact entities.
@@ -14,15 +15,41 @@ use Drupal\Core\Url;
  */
 class ContactListBuilder extends EntityListBuilder {
 
+  protected $limit = 10;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function load() {
+    $entity_query = Drupal::service('entity.query')->get('contact');
+    $header = $this->buildHeader();
+    $entity_query->pager($this->limit);
+    $entity_query->tableSort($header);
+    $entity_ids = $entity_query->execute();
+    return $this->storage->loadMultiple($entity_ids);
+  }
+
   /**
    * {@inheritdoc}
    */
   public function buildHeader() {
-    $header['name'] = $this->t('Name');
+    $header['name'] = [
+      'data' => $this->t('Name'),
+      'field' => 'name',
+      'specifier' => 'name'
+    ];
     $header['photo'] = $this->t('Photo');
-    $header['group'] = $this->t('Group');
+    $header['group'] = [
+      'data' => $this->t('Group'),
+      'field' => 'contact_group_id',
+      'specifier' => 'contact_group_id'
+    ];
     $header['phone_number'] = $this->t('Phone Number');
-    $header['email'] = $this->t('Email');
+    $header['email'] = [
+      'data' => $this->t('Email'),
+      'field' => 'email',
+      'specifier' => 'email'
+    ];
     $header['owner'] = $this->t('Owner');
     return $header + parent::buildHeader();
   }
